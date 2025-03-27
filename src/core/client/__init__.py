85f9f213907c2202cabba6fc7bf212a77d581823
lucyscript <wathne.sveinung@gmail.com>
2025-03-27 19:07:09 +0100

    Renaming and prompt enhancements

--- src/core/client/__init__.py
+++ src/core/client/__init__.py
@@ -1,1 +1,5 @@
 """Init."""
+
+from src.core.client import client
+
+__all__ = ["client"]
--- src/platform/__init__.py
+++ src/platform/__init__.py
@@ -1,5 +1,5 @@
 """API package for WhatsApp and Telegram integrations."""
 
-from src.api import telegram, whatsapp
+from src.platform import telegram, whatsapp
 
 __all__ = ["whatsapp", "telegram"]
